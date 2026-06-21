ecec82e8de33258133895b1983a71cb74448619e
WDSalda <what.ds@outlook.com>
2026-06-21 20:26:25 +0200

    Adicionamos los trabajos con descripciones.

--- src/app/data/works.ts
+++ src/app/data/works.ts
@@ -1,7 +1,7 @@
 export interface Work {
   id: number;
   title: string;
-  category: 'Pintura' | 'Cerámica' | 'Escultura' | 'Digital';
+  category: 'Pintura' | 'Cerámica' | 'Escultura' | 'Digital'| 'Ilustración';
   year: string;
   technique: string;
   description: string;
@@ -12,8 +12,10 @@ export const WORKS: Work[] = [
   
   { id: 1, title: 'Equilibrio', category: 'Cerámica', year: '2025', technique: 'Arcilla', description: 'Pieza inspirada en la armonía y la asimetría.', image: 'assets/images/equilibrio.png?auto=format&fit=crop&w=1200&q=80' },
   { id: 2, title: 'Fragmento de la persona', category: 'Escultura', year: '2025', technique: 'Modelado escultórico', description: 'Un rostro hace parte.', image: 'assets/images/mascara.png?auto=format&fit=crop&w=1200&q=80' },
-  { id: 3, title: 'Bodegon Cafe', category: 'Pintura', year: '2025', technique: 'Acrílico sobre lienzo', description: 'El cafe, siempre me identificara como parte de mi identidad Colombiana, no se necesita una bandera solo el aroma.', image: 'assets/images/bodegonCafe.jpg?auto=format&fit=crop&w=1200&q=80' },
-  { id: 4, title: 'Archivo digital', category: 'Digital', year: '2026', technique: 'Composición digital', description: 'Intersección entre arte, memoria visual y tecnología.', image: 'https://images.unsplash.com/photo-1549490349-8643362247b5?auto=format&fit=crop&w=1200&q=80' },
-  { id: 5, title: 'Fragmentos', category: 'Pintura', year: '2025', technique: 'Técnica mixta', description: 'Capas, texturas y gestos como registro emocional.', image: 'https://images.unsplash.com/photo-1541961017774-22349e4a1262?auto=format&fit=crop&w=1200&q=80' },
-  { id: 6, title: 'Materia viva', category: 'Cerámica', year: '2025', technique: 'Cerámica manual', description: 'Pieza que dialoga con la tierra, la forma y la huella.', image: 'https://images.unsplash.com/photo-1493106819501-66d381c466f1?auto=format&fit=crop&w=1200&q=80' }
+  { id: 3, title: 'Frolero', category: 'Cerámica', year: '2025', technique: 'Arcilla', description: 'Un florero con cecado simple y esmalte brillante adentro, se quiere transmitir el color natural de este tipo de archilla y que se note la textura del proceso en el torno.', image: 'assets/images/florero.png?auto=format&fit=crop&w=1200&q=80' },
+  { id: 4, title: 'Gombey', category: 'Ilustración', year: '2026', technique: 'Papel, acuarela y lapices de colores', description: 'Esta ilustración captura, a través de trazos vibrantes y el dinamismo del color, la energía y fluidez de la danza tradicional Gombey. Más que un movimiento, el baile es una poderosa expresión de libertad e identidad cultural', image: 'assets/images/Gombey.jpeg?auto=format&fit=crop&w=1200&q=80' },
+  { id: 5, title: 'Penumbra', category: 'Pintura', year: '2026', technique: 'Acrílico sobre lienzo', description: 'Un estudio de tonos fríos y nocturnos, donde la serenidad del violeta envuelve los objetos en una quietud íntima y melancólica.', image: 'assets/images/Morado.png?auto=format&fit=crop&w=1200&q=80' },
+  { id: 6, title: 'Luz Fluida', category: 'Pintura', year: '2026', technique: 'Acrílico sobre lienzo', description: 'Calidez y energía. Los fondos encendidos irradian una vibración lumínica que parece emanar del propio lienzo', image: 'assets/images/NaranjaPintura.png?auto=format&fit=crop&w=1200&q=80' },
+  { id: 7, title: 'Contraste Primario', category: 'Pintura', year: '2026', technique: 'Acrílico sobre lienzo', description: 'El equilibrio de la serie, donde el contraste del fondo rojizo profundo resalta los volúmenes.', image: 'assets/images/ColoresOriginales.png?auto=format&fit=crop&w=1200&q=80' },
+  { id: 8, title: 'Bodegon Cafe', category: 'Pintura', year: '2025', technique: 'Acrílico sobre lienzo', description: 'El cafe, siempre me identificara como parte de mi identidad Colombiana, no se necesita una bandera solo el aroma.', image: 'assets/images/bodegonCafe.jpg?auto=format&fit=crop&w=1200&q=80' }
 ];
